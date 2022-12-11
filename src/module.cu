5fa805fc15172f7cb7925c8e60f4c2ece42968cd
FraBet29 <frabet29@gmail.com>
2022-12-11 22:36:34 +0100

    Fix bug

--- src/module.cu
+++ src/module.cu
@@ -263,8 +263,10 @@ void CrossEntropyLoss::forward(bool training) {
     timer_start(TMR_LOSS_FW);
     float *cuda_total_loss;
     check_call(cudaMalloc(&cuda_total_loss, sizeof(float)));
+    std::cout << "OK 0" << std::endl;
     int *cuda_count;
     check_call(cudaMalloc(&cuda_count, sizeof(int)));
+    std::cout << "OK 1" << std::endl;
     if (training) cuda_logits->zero_grad();
     // GPU blocks and threads settings
     dim3 blocksPerGrid1((logits->data.size() / num_classes + MAX_THREADS_PER_BLOCK_1D - 1) / MAX_THREADS_PER_BLOCK_1D, 1, 1);
@@ -282,7 +284,9 @@ void CrossEntropyLoss::forward(bool training) {
         cudaDeviceSynchronize();
     }
     check_call(cudaFree(cuda_total_loss));
+    std::cout << "OK 2" << std::endl;
     check_call(cudaFree(cuda_count));
+    std::cout << "OK 3" << std::endl;
     /*
     float total_loss = 0;
     int count = 0;
