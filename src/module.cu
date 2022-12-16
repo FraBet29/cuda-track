6b6666767e84376e27f30dcb160c2e45dcbb832a
FraBet29 <frabet29@gmail.com>
2022-12-16 12:29:00 +0100

    FIx bug

--- src/module.cu
+++ src/module.cu
@@ -481,7 +481,7 @@ void Dropout::forward(bool training) {
     dim3 blocksPerGrid((cuda_in->size + MAX_THREADS_PER_BLOCK_1D - 1) / MAX_THREADS_PER_BLOCK_1D, 1, 1);
     dim3 threadsPerBlock(MAX_THREADS_PER_BLOCK_1D, 1, 1);
     // Launch kernel
-    dropout_forward_parallel<<<blocksPerGrid, threadsPerBlock>>>(cuda_in->data, cuda_mask, cuda_in->size, threshold, scale, cuda_rand_state, MY_CUDA_RAND_MAX);
+    dropout_forward_parallel<<<blocksPerGrid, threadsPerBlock>>>(cuda_in->data, cuda_mask, cuda_in->size, threshold, scale, cuda_rand_state, MY_RAND_MAX);
     check_kernel_call();
     cudaDeviceSynchronize();
     /*
