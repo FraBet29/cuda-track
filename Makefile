8a71da5039eeecd04cacf84ac7c14fe5819cbf7d
FraBet29 <frabet29@gmail.com>
2022-12-20 00:29:44 +0100

    Parallel glorot

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CXX=nvcc
 CXXFLAGS=-O3
 LDFLAGS=-lm -lstdc++
 
-CXXFILES=src/gcn.cu src/optim.cu src/module.cu src/parser.cpp src/rand.cpp src/timer.cpp src/cuda_check.cu src/cuda_rand.cu src/cuda_variable.cu src/cuda_sparse.cu
-HFILES=include/gcn.h include/optim.h include/module.h include/sparse.h include/parser.h include/rand.h include/timer.h include/cuda_check.h include/cuda_rand.h include/cuda_variable.h include/cuda_sparse.h
+CXXFILES=src/gcn.cu src/optim.cu src/module.cu src/parser.cpp src/timer.cpp src/cuda_check.cu src/cuda_rand.cu src/cuda_variable.cu src/cuda_sparse.cu
+HFILES=include/gcn.h include/optim.h include/module.h include/sparse.h include/parser.h include/timer.h include/cuda_check.h include/cuda_rand.h include/cuda_variable.h include/cuda_sparse.h
 
 
 all: gcn-seq
--- src/cuda_variable.cu
+++ src/cuda_variable.cu
@@ -1,6 +1,6 @@
 #include "../include/cuda_variable.h"
-#include "../include/rand.h"
 #include "../include/cuda_check.h"
+#include "../include/cuda_rand.h"
 #include <cmath>
 #include <cstdio>
 #include <algorithm>
@@ -21,15 +21,33 @@ CudaVariable::~CudaVariable() {
 
 /**
  * Glorot (Xavier) method for weights initialization
- * CAN BE PARALLELIZED (CUDA RANDOM GENERATOR NEEDED)
 */
+
+__global__ void glorot_parallel(float *data, float range, int size, curandState *rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i < size)
+        data[i] = (curand_uniform(&rand_state[i]) / MY_RAND_MAX - 0.5) * range * 2;
+}
+
 void CudaVariable::glorot(int in_size, int out_size) {
+    curandState *cuda_rand_state;
+    // Initialize CUDA random
+    check_call(cudaMalloc(&cuda_rand_state, size * sizeof(curandState)));
+    rand_setup_kernel<<<blocksPerGrid, threadsPerBlock>>>(cuda_rand_state, size);
+    check_kernel_call();
+    cudaDeviceSynchronize();
+    float range = sqrtf(6.0f / (in_size + out_size)); 
+    glorot_parallel<<<(size + MAX_NUM_THREADS - 1) / MAX_NUM_THREADS, MAX_NUM_THREADS>>>(data, range, size, cuda_rand_state);
+    check_kernel_call();
+    cudaDeviceSynchronize();
+    /*
     float range = sqrtf(6.0f / (in_size + out_size));
     float *temp_data = (float *) malloc(size * sizeof(float));
     for(int i = 0; i < size; ++i)
         temp_data[i] = (float(RAND()) / MY_RAND_MAX - 0.5) * range * 2;
     check_call(cudaMemcpy(data, temp_data, size * sizeof(float), cudaMemcpyHostToDevice));
     free(temp_data);
+    */
 }
 
 __global__ void zero_parallel(float *data, int size) {
@@ -60,7 +78,7 @@ __device__ float warp_reduce(float val) {
 
 __global__ void grad_norm_parallel(float *in, float *out, int size) {
     int warp_size = 32;
-    float sum = float(0);
+    float sum = 0.0f;
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += blockDim.x * gridDim.x)
         sum += in[i] * in[i];
     sum = warp_reduce(sum);
