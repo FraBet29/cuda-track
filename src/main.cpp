b15dc9b01fb0625d76e46e56af4de5225bcf3bec
FraBet29 <frabet29@gmail.com>
2022-12-22 09:41:09 +0100

    Add GPU memory info

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include "../include/module.h"
 #include "../include/optim.h"
 #include "../include/parser.h"
+#include "cuda.h"
+#include "cuda_runtime_api.h"
 
 using namespace std;
-
 
 int main(int argc, char **argv) {
     setbuf(stdout, NULL);
@@ -23,8 +25,28 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    // Get gpu info
+    int num_gpus;
+    std::size_t free, total;
+    cudaGetDeviceCount(&num_gpus);
+    for (int gpu_id = 0; gpu_id < num_gpus; gpu_id++) {
+        cudaSetDevice(gpu_id);
+        int id;
+        cudaGetDevice(&id);
+        cudaMemGetInfo(&free, &total);
+        std::cout << "GPU " << id << " memory: free = " << free << ", total = " << total << std::endl;
+    }
 
     GCN gcn(params, &data); // Create and initialize and object of type GCN.
     gcn.run(); // Run the main function of the model in order to train and validate the solution.
+
+    for (int gpu_id = 0; gpu_id < num_gpus; gpu_id++) {
+        cudaSetDevice(gpu_id);
+        int id;
+        cudaGetDevice(&id);
+        cudaMemGetInfo(&free);
+        std::cout << "GPU " << id << " memory: free = " << free << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
