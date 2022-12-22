08f88888baf7ab411cb445ee2e4d8f9ead5bc045
FraBet29 <frabet29@gmail.com>
2022-12-22 10:07:45 +0100

    Fix bug

--- src/main.cu
+++ src/main.cu
@@ -36,13 +36,13 @@ int main(int argc, char **argv) {
         cudaGetDeviceProperties(&prop, i);
         std::cout << "Device Number: " << i << std::endl;
         std::cout << "  Device name: " << prop.name << std::endl;
-        std::cout << "  Global memory (GB): " << (float) prop.totalGlobalMem << std::endl;
-        std::cout << "  Shared memory per block (KB) " << (float) prop.sharedMemPerBlock << std::endl;
+        std::cout << "  Global memory (GB): " << (float) prop.totalGlobalMem / 1.0e9 << std::endl;
+        std::cout << "  Shared memory per block (KB) " << (float) prop.sharedMemPerBlock / 1.0e3 << std::endl;
         std::cout << "  Warp-size: " << prop.warpSize << std::endl;
         std::cout << "  Maximum number of threads per block: " << prop.maxThreadsPerBlock << std::endl;
-        std::cout << "  Memory Clock Rate (MHz): " << prop.memoryClockRate << std::endl;
+        std::cout << "  Memory Clock Rate (MHz): " << prop.memoryClockRate / 1.0e3 << std::endl;
         std::cout << "  Memory Bus Width (bits): " << prop.memoryBusWidth << std::endl;
-        std::cout << "  Peak Memory Bandwidth (GB/s): " << 2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6 << std::endl;
+        std::cout << "  Peak Memory Bandwidth (GB/s): " << 2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8.0) / 1.0e6 << std::endl;
     }
 
     GCN gcn(params, &data); // Create and initialize and object of type GCN.
