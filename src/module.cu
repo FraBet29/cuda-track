67448131de8bbc1d1e455812f60e484a9366cf02
FraBet29 <frabet29@gmail.com>
2022-12-27 09:36:06 +0100

    Fix bug

--- src/module.cu
+++ src/module.cu
@@ -24,10 +24,10 @@ __global__ void matmul_forward_parallel(float *a, float *b, float *c, int m, int
         float sum = 0.0f;
         for (int k = 0; threadIdx.x + k < n; k += TILE_SIZE)
             a_tile[threadIdx.y * n + threadIdx.x + k] = a[i * n + threadIdx.x + k];
-        /*
         for (int k = 0; threadIdx.y + k < n; k += TILE_SIZE)
             b_tile[(threadIdx.y + k) * p + threadIdx.x] = b[(threadIdx.y + k) * p + j];
         __syncthreads();
+        /*
         for (int k = 0; k < n; ++k)
             sum += a_tile[i * n + k] * b_tile[k * p + j];
         c[i * p + j] = sum;
